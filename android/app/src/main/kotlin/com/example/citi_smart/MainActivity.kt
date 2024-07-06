c218889e9c5c8217cd02e32177bbe73131d5f977
pemapog022 <pemapog022@student.wethinkcode.co.za>
2024-07-06 16:16:28 +0200

    start

---
+++ android/app/src/main/kotlin/com/example/citi_smart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.citi_smart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,6 @@
+import 'package:citi_smart/main.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
