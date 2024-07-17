7e0245d82751fe1986b5c62d2e445dfa9f669185
Comfort Ngwenya <>
2024-07-18 00:48:37 +0200

    fix login

--- lib/pages/login/login_form.dart
+++ lib/pages/login/login_form.dart
@@ -6,7 +6,6 @@ import 'package:citi_smart/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 class LoginForm extends StatefulWidget {
   const LoginForm({super.key});
 
@@ -15,12 +14,9 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
-
-
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
 
   @override
   void dispose() {
@@ -43,118 +39,118 @@ class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-      child: Column(
-        children: [
-          Image.asset(
-              'assets/images/signIn.png'),
-          SizedBox(height: 20),
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            textInputAction: TextInputAction.next,
-            cursorColor: kPrimaryColor,
-            onSaved: (email) {},
-            decoration: const InputDecoration(
-              hintText: "Your email",
-              prefixIcon: Padding(
-                padding: EdgeInsets.all(defaultPadding),
-                child: Icon(Icons.person),
-              ),
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your email';
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-            child: TextFormField(
-              textInputAction: TextInputAction.done,
-              obscureText: true,
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Image.asset(
+                'assets/images/signIn.png'), // Make sure to add your logo image in assets
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
               cursorColor: kPrimaryColor,
               decoration: const InputDecoration(
-                hintText: "Your password",
+                hintText: "Your email",
                 prefixIcon: Padding(
                   padding: EdgeInsets.all(defaultPadding),
-                  child: Icon(Icons.lock),
+                  child: Icon(Icons.person),
                 ),
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter your password';
-                }
-                return null;
-              },
+              validator: _validateEmail,
             ),
-          ),
-          const SizedBox(height: defaultPadding),
-          ElevatedButton(
-            onPressed: () async {
-              if (_formKey.currentState!.validate()) {
-                try {
-                  UserCredential userCredential =
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(
-                    email: emailController.text,
-                    password: passwordController.text,
-                  );
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const HomePage(),
-                    ),
-                  );
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'user-not-found' ||
-                      e.code == 'wrong-password') {
-                    _showErrorDialog(context, 'Incorrect email or password');
-                  } else {
-                    _showErrorDialog(context, e.message ?? 'An error occurred');
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+              child: TextFormField(
+                controller: passwordController,
+                textInputAction: TextInputAction.done,
+                obscureText: true,
+                cursorColor: kPrimaryColor,
+                decoration: const InputDecoration(
+                  hintText: "Your password",
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.all(defaultPadding),
+                    child: Icon(Icons.lock),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            const SizedBox(height: defaultPadding),
+            ElevatedButton(
+              onPressed: () async {
+                if (_formKey.currentState!.validate()) {
+                  try {
+                    print("Email: ${emailController.text}");
+                    print("Password: ${passwordController.text}");
+
+                    UserCredential userCredential =
+                        await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: emailController.text,
+                      password: passwordController.text,
+                    );
+
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HomePage(),
+                      ),
+                    );
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'user-not-found' ||
+                        e.code == 'wrong-password') {
+                      _showErrorDialog(context, 'Incorrect email or password');
+                    } else {
+                      _showErrorDialog(
+                          context, e.message ?? 'An error occurred');
+                    }
                   }
                 }
-              }
-            },
-            child: Text(
-              "Login".toUpperCase(),
+              },
+              child: Text("Login".toUpperCase()),
             ),
-          ),
-          const SizedBox(height: defaultPadding),
-          AlreadyHaveAnAccountCheck(
-            press: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const SignupPage();
-                  },
-                ),
-              );
-            },
-          ),
-        ],
+            const SizedBox(height: defaultPadding),
+            AlreadyHaveAnAccountCheck(
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const SignupPage();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
-}
 
 // class _formKey {}
 
-void _showErrorDialog(BuildContext context, String message) {
-  showDialog(
-    context: context,
-    builder: (ctx) => AlertDialog(
-      title: Text('Error'),
-      content: Text(message),
-      actions: <Widget>[
-        TextButton(
-          child: Text('Okay'),
-          onPressed: () {
-            Navigator.of(ctx).pop();
-          },
-        )
-      ],
-    ),
-  );
+  void _showErrorDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Error'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Okay'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          )
+        ],
+      ),
+    );
+  }
 }
